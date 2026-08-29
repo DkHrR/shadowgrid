056e58f02747570d2b149dcfd9a4799b8694f486
Dhruv Amoldeep Shinde <172916227+DkHrR@users.noreply.github.com>
2026-08-29 17:08:12 +0530

    Fix password policy again

--- run-test.spec.ts
+++ run-test.spec.ts
@@ -67,7 +67,7 @@ test('E2E Runtime verification', async () => {
         privateStateProvider: levelPrivateStateProvider({
             privateStateStoreName: 'test-private-state',
             signingKeyStoreName: 'test-signing-keys',
-            privateStoragePasswordProvider: () => 'test-password-12345',
+            privateStoragePasswordProvider: () => 'shadowgrid-production-password-998877',
             accountId: GENESIS_MINT_WALLET_SEED
         }),
         publicDataProvider: indexerPublicDataProvider(envConfig.indexer, envConfig.indexerWS),
